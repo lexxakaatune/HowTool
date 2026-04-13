c503972bd094a35818e959c931fed6059bf46f2b
Alex Agboli <alexcasimia@gmail.com>
2026-04-13 06:42:02 +0100

    Update FeedbackManagement.tsx

--- src/pages/admin/FeedbackManagement.tsx
+++ src/pages/admin/FeedbackManagement.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Eye, Clock, CheckCircle,Trash2 } from "lucide-react";
+import { Feedback } from "../../data/store";
 import { fetchFeedbacks } from "../../services/api";
  
 
