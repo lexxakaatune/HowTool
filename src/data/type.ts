19bd54de00afe38f25f87012d424e3b58a6c8b07
Alex Agboli <alexcasimia@gmail.com>
2026-04-13 06:43:25 +0100

    Create type.ts

---
+++ src/data/type.ts
@@ -0,0 +1,10 @@
+export interface Feedback {
+  id: string;
+  type: 'request' | 'issue' | 'suggestion';
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+  createdAt: number;
+  status: 'pending' | 'read' | 'resolved';
+}
